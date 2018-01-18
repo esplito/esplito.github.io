24f77fb8fb072b7e75f5e54604a0a02b7b0e0a18
Emil Esplund <eesplund@gmail.com>
2018-01-18 16:42:18 +0100

    Amended JS

--- js/pubg_stats.js
+++ js/pubg_stats.js
@@ -1,7 +1,7 @@
 $.ajax({ 
    type : "GET", 
    url : "https://api.pubgtracker.com/v2/profile/pc/esplito", 
-   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', '2c10d855-5cec-4169-8747-78a8ea6dd37a');},
+   beforeSend: function(xhr){xhr.setRequestHeader('TRN-Api-Key', '2c10d855-5cec-4169-8747-78a8ea6dd37a');},
    success : function(result) { 
        //set your variable to the result
        console.log(result); 
