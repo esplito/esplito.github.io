0165358b1ecbe9779b79e804f6004ab3e9d0b6e1
Emil Esplund <eesplund@gmail.com>
2018-01-18 16:25:52 +0100

    JS for PUBG tracker added

---
+++ js/pubg_stats.js
@@ -0,0 +1,9 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var test = this.responseText;
+  }
+};
+xhttp.open("GET", "https://api.pubgtracker.com/v2/profile/pc/esplito", true);
+xhttp.setRequestHeader("TRN-Api-Key", "2c10d855-5cec-4169-8747-78a8ea6dd37a");
+xhttp.send();
