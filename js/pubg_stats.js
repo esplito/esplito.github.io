77d68057a90696463c0e0fd1d2659442e10592ec
Emil Esplund <eesplund@gmail.com>
2018-01-18 16:28:50 +0100

    amended error in js

--- js/pubg_stats.js
+++ js/pubg_stats.js
@@ -5,5 +5,5 @@ xhttp.onreadystatechange = function() {
   }
 };
 xhttp.open("GET", "https://api.pubgtracker.com/v2/profile/pc/esplito", true);
-xhttp.setRequestHeader("TRN-Api-Key", "2c10d855-5cec-4169-8747-78a8ea6dd37a");
+xhttp.setRequestHeader("TRN-Api-Key:", "2c10d855-5cec-4169-8747-78a8ea6dd37a");
 xhttp.send();
