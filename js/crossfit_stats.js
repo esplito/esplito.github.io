fb2c13f905359259978e62e6c89e39f8a7e9329b
Emil Esplund <eesplund@gmail.com>
2018-01-24 17:39:46 +0100

    Added second source for Crossfit data

--- js/crossfit_stats.js
+++ js/crossfit_stats.js
@@ -1,3 +1,7 @@
 $.getJSON('https://cors.io/?https://openg.azurewebsites.net/api/leaderboard?division=1&sort=0&region=0&stage=5&year=16&page=1&numberperpage=100&scaled=0&occupation=0',function(data){
 	console.log(data);
 });
+
+$.getJSON('https://cors.io/?https://ypzgtalo10.execute-api.us-east-1.amazonaws.com/prod/CrossfitOpen2017?affiliate=14232',function(data){
+	console.log(data);
+});
