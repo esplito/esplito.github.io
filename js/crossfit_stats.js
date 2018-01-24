66582b4f340cf54989d022b10224372b4094087c
Emil Esplund <eesplund@gmail.com>
2018-01-24 17:28:20 +0100

    Testing CORS proxy

--- js/crossfit_stats.js
+++ js/crossfit_stats.js
@@ -1,14 +1,3 @@
-$.ajax({ 
-   type : "GET", 
-   url : "https://openg.azurewebsites.net/api/leaderboard?division=1&sort=0&region=0&stage=5&year=16&page=1&numberperpage=100&scaled=0&occupation=0", 
-   beforeSend: function(xhr){
-    
-   },
-   success : function(result) { 
-       //set your variable to the result
-       console.log(result); 
-   }, 
-   error : function(result) { 
-     //handle the error 
-   } 
- }); 
+$.getJSON('https://cors.io/?u=https://openg.azurewebsites.net/api/leaderboard?division=1&sort=0&region=0&stage=5&year=16&page=1&numberperpage=100&scaled=0&occupation=0',function(data){
+	console.log(data);
+});
