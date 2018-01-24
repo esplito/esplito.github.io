c75e6855c69c89de6e4799ad6f8a9236892a5677
Emil Esplund <eesplund@gmail.com>
2018-01-24 17:30:56 +0100

    Changed proxy parameter

--- js/crossfit_stats.js
+++ js/crossfit_stats.js
@@ -1,3 +1,3 @@
-$.getJSON('https://cors.io/?u=https://openg.azurewebsites.net/api/leaderboard?division=1&sort=0&region=0&stage=5&year=16&page=1&numberperpage=100&scaled=0&occupation=0',function(data){
+$.getJSON('https://cors.io/?https://openg.azurewebsites.net/api/leaderboard?division=1&sort=0&region=0&stage=5&year=16&page=1&numberperpage=100&scaled=0&occupation=0',function(data){
 	console.log(data);
 });
