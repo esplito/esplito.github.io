6474fbafcad70fb701d2da5c6e77195aaee16f6f
Emil Esplund <eesplund@gmail.com>
2018-01-24 16:57:04 +0100

    Changed request URL for Crossfit API

--- js/crossfit_stats.js
+++ js/crossfit_stats.js
@@ -1,6 +1,6 @@
 $.ajax({ 
    type : "GET", 
-   url : "http://openg.azurewebsites.net/api/leaderboard?division=1&sort=0&region=0&stage=5&year=16&page=1&numberperpage=100&scaled=0&occupation=0", 
+   url : "https://openg.azurewebsites.net/api/leaderboard?division=1&sort=0&region=0&stage=5&year=16&page=1&numberperpage=100&scaled=0&occupation=0", 
    beforeSend: function(xhr){
     
    },
