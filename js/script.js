06cb7a07efdf0f8096c2c17ea71c12d86d025c1b
esplito <eesplund@gmail.com>
2018-02-04 21:07:10 +0100

    Youtube iframe and JS added

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+var onYouTubeIframeAPIReady = function () {
+  player = new YT.Player('player', {
+    height: '315',
+    width: '560',
+    videoId: 'dX31eoWSUso',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}();
+
+var arrowClick = function () {
+    
+}(); */
